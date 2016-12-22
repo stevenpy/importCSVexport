cf16d14b3cae803b86ab664cee14d307e2199f8b
stevenpy <stevenphilbert@gmail.com>
2016-12-22 02:08:54 +0100

    Export data to csv works

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,11 @@
 class SalesController < ApplicationController
   def index
 		@sales = Sale.all
+
+		respond_to do |format|
+			format.html
+			format.csv { send_data @sales.to_csv, filename: "sales-#{Date.today}.csv" }
+		end
 	end
 
 	def import
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -13,6 +13,18 @@ class Sale < ApplicationRecord
 		counter
 	end
 
+	def self.to_csv
+    attributes = %w{email first_name last_name}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |sale|
+        csv << attributes.map{ |attr| sale.send(attr) }
+      end
+    end
+  end
+
 	def self.assign_from_row(row)
 		sale = Sale.where(email: row[:email]).first_or_initialize
 		sale.assign_attributes row.to_hash.slice(:first_name, :last_name)
