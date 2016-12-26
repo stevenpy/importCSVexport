9490dd76133e1bdb00a0ea154227dcbe1c3398fe
stevenpy <stevenphilbert@gmail.com>
2016-12-26 15:38:42 +0100

    Solving indentation issue

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -14,20 +14,20 @@ class Sale < ApplicationRecord
 	end
 
 	def self.to_csv
-		attributes = %w{numero_billet commande reservation date_reservation heure_reservation cle_spectacle spectacle cle_representation date_representation heure_representation date_fin_representation heure_fin_representation prix date_acces heure_acces tarif type_de_client type_de_produit serie etage filiere_de_vente nom prenom email adresse code_postal pays age sexe}
+    attributes = %w{numero_billet commande reservation date_reservation heure_reservation cle_spectacle spectacle cle_representation date_representation heure_representation date_fin_representation heure_fin_representation prix date_acces heure_acces tarif type_de_client type_de_produit serie etage filiere_de_vente nom prenom email adresse code_postal pays age sexe}
 
-		CSV.generate(headers: true) do |csv|
-			csv << attributes
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
 
-			all.each do |sale|
-				csv << attributes.map{ |attr| sale.send(attr) }
-			end
+      all.each do |sale|
+        csv << attributes.map{ |attr| sale.send(attr) }
+      end
     end
   end
 
   def self.assign_from_row(row)
-  	sale = Sale.where(numero_billet: row[:numero_billet]).first_or_initialize
-  	sale.assign_attributes row.to_hash.slice(:commande, :reservation, :date_reservation, :heure_reservation, :cle_spectacle, :spectacle, :cle_representation, :date_representation, :heure_representation, :date_fin_representation, :heure_fin_representation, :prix, :date_acces, :heure_acces, :tarif, :type_de_client, :type_de_produit, :serie, :etage, :filiere_de_vente, :nom, :prenom, :email, :adresse, :code_postal, :pays, :age, :sexe)
-  	sale
+    sale = Sale.where(numero_billet: row[:numero_billet]).first_or_initialize
+    sale.assign_attributes row.to_hash.slice(:commande, :reservation, :date_reservation, :heure_reservation, :cle_spectacle, :spectacle, :cle_representation, :date_representation, :heure_representation, :date_fin_representation, :heure_fin_representation, :prix, :date_acces, :heure_acces, :tarif, :type_de_client, :type_de_produit, :serie, :etage, :filiere_de_vente, :nom, :prenom, :email, :adresse, :code_postal, :pays, :age, :sexe)
+    sale
   end
 end
